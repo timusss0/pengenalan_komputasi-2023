4681301b0e8a4d3b9894e934df74ec878e3f4125
timusss0 <mustikavia86@gmail.com>
2023-10-11 14:39:35 +0700

    perulangan for bilangan genap

--- looping7/main.c
+++ looping7/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
+//    for (int satu = 1; satu < 10; ++satu) {
+//        printf("%d",satu);
+//
+//    }
+////    for (int satu = 10; satu > 0; --satu) {
+//        printf("%d\n",satu);
+//
+//    }
+
+//    int batasMaksimal;
+//
+//    printf("Masukkan batas maksimal: ");
+//    scanf("%d", &batasMaksimal);
+//
+//        for (int i = 2; i <= batasMaksimal; i += 2) {
+//            printf("%d\n", i);
+//    }
+
 
     return 0;
 }
